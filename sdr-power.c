67860cfee07a36a0fdaa1661cb2f91879cc94e88
Jan Dohl <jan@legxaro.de>
2016-05-04 00:03:21 +0200

    License headers

--- sdr-power.c
+++ sdr-power.c
@@ -1,8 +1,9 @@
 /*
- * rtl-sdr, turns your Realtek RTL2832 based DVB dongle into a SDR receiver
+ * sdr-power, a port of rtl-power for other SDRs
  * Copyright (C) 2012 by Steve Markgraf <steve@steve-m.de>
  * Copyright (C) 2012 by Hoernchen <la@tfc-server.de>
  * Copyright (C) 2012 by Kyle Keen <keenerd@gmail.com>
+ * Copyright (C) 2016 by Jan Dohl <polygon@c3d2.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
